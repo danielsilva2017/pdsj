48f4c5282e319c782b029547e7c16a17713c26f9
danielsilva2017 <silvadaniel@ua.pt>
2018-11-07 16:11:38 +0000

    distribution done

--- PDSJ/src/pdsj/View.java
+++ PDSJ/src/pdsj/View.java
@@ -25,27 +25,42 @@ public class View {
         Opcao op1, op2, op3, op4,op5;  
         
         //Menu principal
-        List<Opcao> linhas = Arrays.asList(op1, op2, op3);
-        Menu menuAlunos = new Menu(linhas, "   Menu principal");        
-        menusSIGCA.addMenu(1, menuAlunos);
         op1 = new Opcao("Calculadora de datas ", "C");
         op2 = new Opcao("Fuso Horarios", "F");
         op3 = new Opcao("Agenda ", "A"); 
+        List<Opcao> linhas = Arrays.asList(op1, op2, op3);
+        Menu menuAlunos = new Menu(linhas, "   Menu principal");        
+        menusSIGCA.addMenu(1, menuAlunos);
+        
         
         //Calculadora
         List<Opcao> linhas2 = Arrays.asList(op1, op2, op3);
         Menu menuAlunos2 = new Menu(linhas2, "   Menu principal");        
         menusSIGCA.addMenu(2, menuAlunos2);
-        return menusSIGCA;
+       
         
         
         
         
         //Fuso horario
-        
+        List<Opcao> linhas20 = Arrays.asList(op1, op2, op3);
+        Menu menuAlunos20 = new Menu(linhas2, "   Menu principal");        
+        menusSIGCA.addMenu(20, menuAlunos2);
         
         
         
         //Agenda
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+         return menusSIGCA;
      }
 }
