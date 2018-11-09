20b909f6da379ae1291092fc988f206b21f5306d
danielsilva2017 <silvadaniel@ua.pt>
2018-11-09 09:15:56 +0000

    files changed

--- PDSJ/src/pdsj/Controller.java
+++ PDSJ/src/pdsj/Controller.java
@@ -37,6 +37,57 @@ public class Controller {
     
     //Metodos Fuso Horario
     
+    public void horasPais(){
+       System.out.println("Qual é o id do Pais ");
+       String zona= Input.lerStringZona();
+       model.horasPais(zona);
+   }    
+   public void diferençaHoras(){
+      System.out.println("Qual é o id do Pais 1 ");
+       String zona= Input.lerStringZona();
+       System.out.println("Qual é o id do Pais 2 ");
+       String zona2= Input.lerStringZona();
+       model.diferençaHoras(zona,zona2);
+   }
+   public void queHorasSerao(){
+       int year =0;
+       Month month = Month.JANUARY;
+       int day =0;
+        System.out.println("Insira a data de acordo com a resolução escolhida no ficheiro de configuração");
+        String data= Input.lerString();
+        String [] date=parseDate(data);
+        if(formatDate=="Common"){
+            day = Integer.parseInt(date[0]);
+            int auxMonth= Integer.parseInt(date[1]);
+            month = getMonth(auxMonth);
+            year = Integer.parseInt(date[2]);
+        }
+        else{
+            year = Integer.parseInt(date[0]);
+            int auxMonth= Integer.parseInt(date[1]);
+            month = getMonth(auxMonth);
+            day= Integer.parseInt(date[2]);
+        }   
+        System.out.println(day+"-"+month+"-"+year);
+        System.out.println("Insira hora de partida no formato HH:MM");
+        String horaString= Input.lerString();
+        String [] horaStringRes= parseHours(horaString);
+        int hora= Integer.parseInt(horaStringRes[0]);
+        int minutos=Integer.parseInt(horaStringRes[1]);
+        System.out.println("Insira zona de partida");
+        String zonaP= Input.lerStringZona();
+        System.out.println("Insira zona de chegada");
+        String zonaC= Input.lerStringZona();
+        System.out.println("Insira a duração da viagem  no formato HH:MM");
+        String horaVString= Input.lerString();
+        String [] horaVStringRes= parseHours(horaVString);
+        int horaV= Integer.parseInt(horaVStringRes[0]);
+        int minutosV= Integer.parseInt(horaVStringRes[1]);
+        System.out.println(year+""+ ""+month+ day+ hora+minutos+zonaP+zonaC+horaV+minutosV);
+        
+        model.queHorasSerao(year, month, day, hora, minutos, zonaP, zonaC, horaV, minutosV);
+   }
+    
     
     
     //Metodos Agenda
@@ -102,56 +153,7 @@ public class Controller {
         }
         while(!opcao.equals("S"));    
    }
-   public void horasPais(){
-       System.out.println("Qual é o id do Pais ");
-       String zona= Input.lerStringZona();
-       model.horasPais(zona);
-   }    
-   public void diferençaHoras(){
-      System.out.println("Qual é o id do Pais 1 ");
-       String zona= Input.lerStringZona();
-       System.out.println("Qual é o id do Pais 2 ");
-       String zona2= Input.lerStringZona();
-       model.diferençaHoras(zona,zona2);
-   }
-   public void queHorasSerao(){
-       int year =0;
-       Month month = Month.JANUARY;
-       int day =0;
-        System.out.println("Insira a data de acordo com a resolução escolhida no ficheiro de configuração");
-        String data= Input.lerString();
-        String [] date=parseDate(data);
-        if(formatDate=="Common"){
-            day = Integer.parseInt(date[0]);
-            int auxMonth= Integer.parseInt(date[1]);
-            month = getMonth(auxMonth);
-            year = Integer.parseInt(date[2]);
-        }
-        else{
-            year = Integer.parseInt(date[0]);
-            int auxMonth= Integer.parseInt(date[1]);
-            month = getMonth(auxMonth);
-            day= Integer.parseInt(date[2]);
-        }   
-        System.out.println(day+"-"+month+"-"+year);
-        System.out.println("Insira hora de partida no formato HH:MM");
-        String horaString= Input.lerString();
-        String [] horaStringRes= parseHours(horaString);
-        int hora= Integer.parseInt(horaStringRes[0]);
-        int minutos=Integer.parseInt(horaStringRes[1]);
-        System.out.println("Insira zona de partida");
-        String zonaP= Input.lerStringZona();
-        System.out.println("Insira zona de chegada");
-        String zonaC= Input.lerStringZona();
-        System.out.println("Insira a duração da viagem  no formato HH:MM");
-        String horaVString= Input.lerString();
-        String [] horaVStringRes= parseHours(horaVString);
-        int horaV= Integer.parseInt(horaVStringRes[0]);
-        int minutosV= Integer.parseInt(horaVStringRes[1]);
-        System.out.println(year+""+ ""+month+ day+ hora+minutos+zonaP+zonaC+horaV+minutosV);
-        
-        model.queHorasSerao(year, month, day, hora, minutos, zonaP, zonaC, horaV, minutosV);
-   }
+   
         
      
      
